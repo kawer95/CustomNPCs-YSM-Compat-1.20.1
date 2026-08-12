dbe9901c0dee9f6ae4631dcb0858c3ad0e5208cd
kawer95 <1034765569@qq.com>
2026-08-12 23:50:05 +0800

    Trace all armed NPC movement on the client

--- src/main/java/com/arxyt/customnpcsysmcompat/client/AnimatedNpcRenderBridge.java
+++ src/main/java/com/arxyt/customnpcsysmcompat/client/AnimatedNpcRenderBridge.java
@@ -279,12 +279,13 @@ public final class AnimatedNpcRenderBridge {
     private static void traceRetreatRender(AnimatedProxy holder, EntityNPCInterface npc,
                                            NpcMovementTracker.Sample movement,
                                            boolean backpedalling, float targetBodyYaw) {
-        if (!backpedalling && !holder.wasBackpedalling) return;
-        if (backpedalling && holder.wasBackpedalling && npc.tickCount % 5 != 0) return;
+        boolean armedMovement = movement.walking() && GunCompat.active(npc);
+        if (!armedMovement && !backpedalling && !holder.wasBackpedalling) return;
+        if (npc.tickCount % 5 != 0 && backpedalling == holder.wasBackpedalling) return;
         float difference = net.minecraft.util.Mth.wrapDegrees(movement.movementYaw() - npc.yHeadRot);
         CustomNpcsYsmCompat.LOGGER.info(
-                "[YSM-RETREAT-TRACE][CLIENT-PROXY] npcId={} tick={} backpedalling={} walking={} speed={} movementYaw={} npcRotation={} npcBodyYaw={} npcHeadYaw={} directionDifference={} selectedProxyBodyYaw={} previousProxyBodyYaw={}",
-                npc.getId(), npc.tickCount, backpedalling, movement.walking(), movement.speed(),
+                "[YSM-RETREAT-TRACE][CLIENT-PROXY] npcId={} tick={} armedMovement={} backpedalling={} walking={} speed={} movementYaw={} npcRotation={} npcBodyYaw={} npcHeadYaw={} directionDifference={} selectedProxyBodyYaw={} previousProxyBodyYaw={}",
+                npc.getId(), npc.tickCount, armedMovement, backpedalling, movement.walking(), movement.speed(),
                 movement.movementYaw(), npc.getYRot(), npc.yBodyRot, npc.yHeadRot, difference,
                 targetBodyYaw, holder.orientation.bodyYaw());
         holder.wasBackpedalling = backpedalling;
